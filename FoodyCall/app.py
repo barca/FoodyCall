756d2b4cbb6245c361e22a45234a50eb8a799812
mtrifunovski <mtrifunovski@wesleyan.edu>
2014-09-05 14:21:19 -0400

    Stupid changes.

--- FoodyCall/app.py
+++ FoodyCall/app.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def FoodyCall():
-    return "Hey fatasses!"
+def login():
+    return "Enter your cell # here!"
+
+@app.route('/menu')
+def menu():
+    return "Food goes here"
+
+@app.route('/sides')
+def sides():
+    return "Sides go here"
 
 if __name__ == '__main__':
     app.run()
