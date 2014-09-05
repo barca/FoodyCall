ac3a85c5a06a7ef0fa9baa33de26f693d35c8a2c
mtrifunovski <mtrifunovski@wesleyan.edu>
2014-09-05 19:51:57 -0400

    Ratings system

---
+++ FoodyCall/lib/ratings/ratings.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from flask import Blueprint
+from flask import jsonify
+from flask import make_response
+from flask import request
+from lib.menu.menu import MongoClient
+from bson.objectid import ObjectId
+import json
+
+# Create Blueprint
+ratings = Blueprint('ratings', __name__, template_folder='templates')
+client = MongoClient()
+db = client.menudb
+menu_items=db.menu_items
+
+@ratings.route('',methods=['POST'])
+def index():
+		item_id = request.form.get("item_id")
+		rating = request.form.get("rate")
+		avg = {}
+		for item in menu_items.find():
+	 		if item_id == str(item['_id']):
+	 			avg = (item['rating_avg']*item['rater_count']+rating)/(item['rater_count']+1)
+	 	if avg:
+	 	  db.menu.update({item_id: item_id}, 
+	 		  {
+	 			  '$set': { rating_avg: avg, 
+	 				  			rater_count: rater_count+1 
+	 					  	}
+	 		  })
+	 	return json.dumps(avg)
