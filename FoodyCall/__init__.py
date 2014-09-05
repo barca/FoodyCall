8cd1b7c047c6aa89f4fcedb2ad31db4b8f4539ab
kault <kault@wesleyan.edu>
2014-09-05 17:05:49 -0400

    Add csv parsing for mongo

--- FoodyCall/__init__.py
+++ FoodyCall/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from lib.menu.menu import menu
 from lib.utils.utils import utils
+from pymongo import MongoClient
+import csv
+
 app = Flask(__name__)
 
-# Init blueprints
+# Initialize blueprints
 app.register_blueprint(menu, url_prefix='/menu')
 app.register_blueprint(utils, url_prefix='/utils')
+
+# Initialize connection with mongo :^)
+# This connects to the default port and host, localhost and 27017
+client = MongoClient()
+
+# Get the database
+db = client.menudb
+
+# Get the collection of items
+menu_items = db.menu_items
+
+summies_menu = csv.DictReader(open("summies.csv"))
+for row in summies_menu:
+	menu_items_id = menu_items.insert(row)
+
 
 @app.route('/')
 def login():
