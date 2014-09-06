18fd4848be813733638a3cb0fd2704a84bc42a31
kault <kault@wesleyan.edu>
2014-09-06 19:12:35 -0400

    Bugfix: only return proper menu based on time

--- FoodyCall/lib/menu/menu.py
+++ FoodyCall/lib/menu/menu.py
@@ -15,16 +15,36 @@ menu = Blueprint('menu', __name__, template_folder='templates')
 # Initialize connection with mongo :^)
 # This connects to the default port and host, localhost and 27017.
 client = MongoClient()
-
 db = client.menudb
 menu_items = db.menu_items
+
+menu_to_serve = "none"
+
+# Get the time and choose which menu to load, then get the collection of items
+now = datetime.now()
+if (now.hour >= 22 and now.hour <= 23) or now.hour == 0:
+	menu_to_serve = "latenight"
+elif (now.hour >= 11 and now.hour <= 2):
+	menu_to_serve = "summies"
+else:
+	print "Invalid time: defaulting to summies for dev purposes"
+	menu_to_serve = "summies"
 
 @menu.route('', methods = ['GET'])
 def index():
  	ret = []
 	for item in menu_items.find():
- 		item['_id'] = str(item['_id'])
- 		ret.append(item)
+		if menu_to_serve == "summies":
+			if item["menu"] == "s":
+				item['_id'] = str(item['_id'])
+ 				ret.append(item)
+		elif menu_to_serve == "latenight":
+			if item["menu"] == "ln":
+				item['_id'] = str(item['_id'])
+ 				ret.append(item)
+		else:
+			print "Error"
+			sys.exit(1)
 
  	return json.dumps(ret)
 
