6c8ae3c0080e09221eb62a0ad3df563baa1e0ea7
mtrifunovski <mtrifunovski@wesleyan.edu>
2014-09-05 20:30:42 -0400

    Tylers additions

--- FoodyCall/__init__.py
+++ FoodyCall/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from lib.menu.menu import menu
 from lib.ratings.ratings import ratings
+from lib.order.order import order
 
 app = Flask(__name__)
 
 # Initialize blueprints
 app.register_blueprint(menu, url_prefix='/menu')
 app.register_blueprint(ratings, url_prefix='/ratings')
+app.register_blueprint(order, url_prefix='/ratings')
 
 @app.route('/')
 def login():
