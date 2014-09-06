497f33fe01071c70d77234af9d8dfd1754a30dbb
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-09-06 13:31:24 -0400

    template added

--- FoodyCall/__init__.py
+++ FoodyCall/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from lib.menu.menu import menu
 from lib.ratings.ratings import ratings
 from lib.order.order import order
@@ -10,6 +11,10 @@ app.register_blueprint(menu, url_prefix='/menu')
 app.register_blueprint(ratings, url_prefix='/ratings')
 app.register_blueprint(order, url_prefix='/order')
 app.register_blueprint(past_order,url_prefix='/prior')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
